ef68ffb140dddfb240b2385ee1ebe6a980c3087b
lee-jehoon <lee.jehoon@nbt.com>
2020-11-19 00:32:12 +0900

    -

--- model.py
+++ model.py
@@ -301,15 +301,43 @@ class InvertedBlock(nn.Module):
 
         layers = []
         if expand_ratio != 1:
-            layers.append(conv1x1(ch_in, hidden_dim))
+            layers.append(self.conv1x1(ch_in, hidden_dim))
         layers.extend([
             #dw
-            dwise_conv(hidden_dim, stride=stride),
+            self.dwise_conv(hidden_dim, stride=stride),
             #pw
-            conv1x1(hidden_dim, ch_out)
+            self.conv1x1(hidden_dim, ch_out)
         ])
 
         self.layers = nn.Sequential(*layers)
+
+    def dwise_conv(ch_in, stride=1):
+        return (
+            nn.Sequential(
+                #depthwise
+                nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
+                nn.BatchNorm2d(ch_in),
+                nn.ReLU6(inplace=True),
+            )
+        )
+
+    def conv1x1(ch_in, ch_out):
+        return (
+            nn.Sequential(
+                nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
+                nn.BatchNorm2d(ch_out),
+                nn.ReLU6(inplace=True)
+            )
+        )
+
+    def conv3x3(ch_in, ch_out, stride):
+        return (
+            nn.Sequential(
+                nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
+                nn.BatchNorm2d(ch_out),
+                nn.ReLU6(inplace=True)
+            )
+        )
 
     def forward(self, x):
         if self.use_res_connect:
