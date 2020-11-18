50b10046cf5b71f918150d624d95d058435d25cd
lee-jehoon <lee.jehoon@nbt.com>
2020-11-19 00:54:36 +0900

    -

--- model.py
+++ model.py
@@ -49,13 +49,108 @@ class Bottleneck(nn.Module):
 
         return out
 
+##################################################################
+##################################################################
+##################################################################
+
+def dwise_conv(ch_in, stride=1):
+    return (
+        nn.Sequential(
+            #depthwise
+            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
+            nn.BatchNorm2d(ch_in),
+            nn.ReLU6(inplace=True),
+        )
+    )
+
+def conv1x1(ch_in, ch_out):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+def conv3x3(ch_in, ch_out, stride):
+    return (
+        nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU6(inplace=True)
+        )
+    )
+
+class InvertedBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, expand_ratio, stride):
+        super(InvertedBlock, self).__init__()
+
+        self.stride = stride
+        assert stride in [1,2]
+
+        hidden_dim = ch_in * expand_ratio
+
+        self.use_res_connect = self.stride==1 and ch_in==ch_out
+
+        layers = []
+        if expand_ratio != 1:
+            layers.append(conv1x1(ch_in, hidden_dim))
+        layers.extend([
+            #dw
+            dwise_conv(hidden_dim, stride=stride),
+            #pw
+            conv1x1(hidden_dim, ch_out)
+        ])
+
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.layers(x)
+        else:
+            return self.layers(x)
+
+class MobileNetV2(nn.Module):
+    def __init__(self, ch_in=3, n_classes=1000):
+        super(MobileNetV2, self).__init__()
+
+        self.configs=[
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1]
+        ]
+
+        self.stem_conv = conv3x3(ch_in, 32, stride=2)
+
+        layers = []
+        input_channel = 32
+        for t, c, n, s in self.configs:
+            for i in range(n):
+                stride = s if i == 0 else 1
+                layers.append(InvertedBlock(ch_in=input_channel, ch_out=c, expand_ratio=t, stride=stride))
+                input_channel = c
+
+        self.layers = nn.Sequential(*layers)
+        self.last_conv = conv1x1(input_channel, 1280)
+
+    def forward(self, x):
+        x = self.stem_conv(x)
+        x = self.layers(x)
+        x = self.last_conv(x)
+        return x
+
+##################################################################
+##################################################################
+##################################################################
+
 class ModelSpatial(nn.Module):
     # Define a ResNet 50-ish arch
     def __init__(self, block = Bottleneck, layers_scene = [3, 4, 6, 3, 2], layers_face = [3, 4, 6, 3, 2]):
-        mbnet_layers = []
-        mbnet_layers.append(MobileNetV2(ch_in=3, n_classes=1000))
-        self.mbnet = nn.Sequential(*mbnet_layers)
-
         # Resnet Feature Extractor
         self.inplanes_scene = 64
         self.inplanes_face = 64
@@ -82,6 +177,10 @@ class ModelSpatial(nn.Module):
         self.layer3_face = self._make_layer_face(block, 256, layers_face[2], stride=2)
         self.layer4_face = self._make_layer_face(block, 512, layers_face[3], stride=2)
         self.layer5_face = self._make_layer_face(block, 256, layers_face[4], stride=1) # additional to resnet50
+
+        mbnet_layers = []
+        mbnet_layers.append(MobileNetV2(ch_in=3, n_classes=1000))
+        self.mbnet = nn.Sequential(*mbnet_layers)
 
         # attention
         self.attn = nn.Linear(1808, 1*7*7)
@@ -232,104 +331,9 @@ class ModelSpatial(nn.Module):
 
         return gaze_heatmap_pred, torch.mean(attn_weights, 1, keepdim=True), encoding_inout
 
-##################################################################
-##################################################################
-##################################################################
 
-def dwise_conv(ch_in, stride=1):
-    return (
-        nn.Sequential(
-            #depthwise
-            nn.Conv2d(ch_in, ch_in, kernel_size=3, padding=1, stride=stride, groups=ch_in, bias=False),
-            nn.BatchNorm2d(ch_in),
-            nn.ReLU6(inplace=True),
-        )
-    )
 
-def conv1x1(ch_in, ch_out):
-    return (
-        nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=1, padding=0, stride=1, bias=False),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
-        )
-    )
 
-def conv3x3(ch_in, ch_out, stride):
-    return (
-        nn.Sequential(
-            nn.Conv2d(ch_in, ch_out, kernel_size=3, padding=1, stride=stride, bias=False),
-            nn.BatchNorm2d(ch_out),
-            nn.ReLU6(inplace=True)
-        )
-    )
-
-class InvertedBlock(nn.Module):
-    def __init__(self, ch_in, ch_out, expand_ratio, stride):
-        super(InvertedBlock, self).__init__()
-
-        self.stride = stride
-        assert stride in [1,2]
-
-        hidden_dim = ch_in * expand_ratio
-
-        self.use_res_connect = self.stride==1 and ch_in==ch_out
-
-        layers = []
-        if expand_ratio != 1:
-            layers.append(conv1x1(ch_in, hidden_dim))
-        layers.extend([
-            #dw
-            dwise_conv(hidden_dim, stride=stride),
-            #pw
-            conv1x1(hidden_dim, ch_out)
-        ])
-
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x):
-        if self.use_res_connect:
-            return x + self.layers(x)
-        else:
-            return self.layers(x)
-
-class MobileNetV2(nn.Module):
-    def __init__(self, ch_in=3, n_classes=1000):
-        super(MobileNetV2, self).__init__()
-
-        self.configs=[
-            # t, c, n, s
-            [1, 16, 1, 1],
-            [6, 24, 2, 2],
-            [6, 32, 3, 2],
-            [6, 64, 4, 2],
-            [6, 96, 3, 1],
-            [6, 160, 3, 2],
-            [6, 320, 1, 1]
-        ]
-
-        self.stem_conv = conv3x3(ch_in, 32, stride=2)
-
-        layers = []
-        input_channel = 32
-        for t, c, n, s in self.configs:
-            for i in range(n):
-                stride = s if i == 0 else 1
-                layers.append(InvertedBlock(ch_in=input_channel, ch_out=c, expand_ratio=t, stride=stride))
-                input_channel = c
-
-        self.layers = nn.Sequential(*layers)
-        self.last_conv = conv1x1(input_channel, 1280)
-
-    def forward(self, x):
-        x = self.stem_conv(x)
-        x = self.layers(x)
-        x = self.last_conv(x)
-        return x
-
-##################################################################
-##################################################################
-##################################################################
 
 class BottleneckConvLSTM(nn.Module):
     expansion = 4
